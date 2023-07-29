aeb4257131ffa6f15e4396eae3021ce417cb5289
simon despres <simondprs62790@hotmail.fr>
2023-07-29 11:40:14 +0200

    ajout/suppression de tâche à faire (manipulation de state)

--- src/App.jsx
+++ src/App.jsx
@@ -9,28 +9,44 @@ function App() {
     { id: nanoid(8), content: "item 2" },
     { id: nanoid(8), content: "item 3" },
   ])
+  const [todo, setTodo] = useState("")
 
   function deleteTodo(id) {
     setTodoList(todoList.filter(todo => todo.id !== id))
   }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodoList([...todoList, { id: nanoid(), content: todo }])
+    setTodo("")
+  }
   return (
-    <div className="h-screen bg-slate-800">
+    <div className="h-screen">
       <div className="max-w-4xl mx-auto pt-20 px-6">
         <h1 className="text-3xl text-slate-100 mb-4">Ma to-do Liste</h1>
 
-        <form className="mb-10">
-          <label htmlFor="todo-item"
-            className="text-slate-50">Ajouter une chose à faire</label>
-          <input type="text" className="mt-1 block w-full rounded" />
-          <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]">Ajouter</button>
+        <form onSubmit={handleSubmit} className="mb-10">
+          <label htmlFor="todo-item" className="text-slate-50">
+            Ajouter une chose à faire
+          </label>
+          <input type="text"
+            value={todo}
+            onChange={e => setTodo(e.target.value)}
+            className="mt-1 block w-full rounded"
+          />
+          <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]">
+            Ajouter
+          </button>
         </form>
         <ul>
           {todoList.length === 0 && (
             <li className="text-slate-50 text-md">Pas de tâche à faire...</li>
           )}
-          {todoList.map(item => (
-            <ListItem key={item.id} itemData={item} deleteTodo={deleteTodo} />
-          ))}
+          {todoList.length > 0 &&
+            todoList.map(item => (
+              <ListItem key={item.id} itemData={item} deleteTodo={deleteTodo} />
+            ))}
         </ul>
       </div>
     </div>
